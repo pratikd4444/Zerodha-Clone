28f1eece7fc9766fa92191191164add65b83f3ea
Pratik Deshmukh <pratikd4444@gmail.com>
2026-06-11 20:17:45 +0530

    fix: resolved the error in code file

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
-import axios, { all } from "axios";
+import axios, {  } from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
-// import { holdings } from "../data/data";
+
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("https://your-zerodha-backend.onrender.com/allHoldings").then((res) => {
+    axios.get("https://your-zerodha-backend.onrender.com/signup").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
   }, []);
 
-  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
   const labels = allHoldings.map((subArray) => subArray["name"]);
 
   const data = {
@@ -28,21 +28,6 @@ const Holdings = () => {
     ],
   };
 
-  // export const data = {
-  //   labels,
-  //   datasets: [
-  // {
-  //   label: 'Dataset 1',
-  //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  // },
-  //     {
-  //       label: 'Dataset 2',
-  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-  //     },
-  //   ],
-  // };
 
   return (
     <>
